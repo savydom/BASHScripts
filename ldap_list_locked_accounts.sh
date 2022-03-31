ae6d0f0e87c8cce432cfe0e42c3595f3063e4a2f
savydom <46859196+savydom@users.noreply.github.com>
2022-03-31 14:34:20 -0400

    Update ldap_list_locked_accounts.sh

--- ldap_list_locked_accounts.sh
+++ ldap_list_locked_accounts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#ldapsearch -h sscmgt1 -D cn=admin,cn=Administrators,cn=dscc -w - -b "ou=people,dc=sscnola,dc=oob" -s sub "(pwdAccountLockedTime=*)" 1.1
+#ldapsearch -h mgt1 -D cn=admin,cn=Administrators,cn=dscc -w - -b "ou=people,dc=DC,dc=dcc" -s sub "(pwdAccountLockedTime=*)" 1.1
 
 if [ `hostname` != "sscmgt1" ] && [ `hostname` != "sscmgt2" ]
 then
-	echo "Must run from sscmgt1 or sscmgt2"
+	echo "Must run from mgt1 or mgt2"
 	exit
 fi
 
-#/usr/bin/ldapsearch -x -b "ou=people,dc=sscnola,dc=oob" \
-#                -H ldaps://sscmgt2.sscnola.oob  \
+#/usr/bin/ldapsearch -x -b "ou=people,dc=DC,dc=DCC" \
+#                -H ldaps://mgt2.DC.DCC  \
 #                  -s sub "(pwdAccountLockedTime=*)" 1.1 |grep dn
 
 /usr/bin/ldapsearch -LLL -Y external -H ldapi:///  \
