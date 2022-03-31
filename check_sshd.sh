246db396c4942011291f2697a30bacc09a809ccb
savydom <46859196+savydom@users.noreply.github.com>
2022-03-31 14:22:01 -0400

    Update check_sshd.sh

--- check_sshd.sh
+++ check_sshd.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-HL=/home/mjohnson/bin/hostlist.txt
-echo 'SSHD Log' > /home/mjohnson/bin/upcheck_sshd_log.txt
+HL=/home/%userprofile%/bin/hostlist.txt
+echo 'SSHD Log' > /home/%userprofile%/bin/upcheck_sshd_log.txt
 for i in `/usr/bin/cat $HL`
     do
          echo "$i"
-         echo "$i" >> /home/mjohnson/bin/check_sshd_log.txt
-         ssh -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i '/usr/bin/sudo /usr/bin/egrep "(oracle|young|williams|walga|bari|kocha)" /etc/ssh/sshd_config' >> /home/mjohnson/bin/check_sshd_log.txt
+         echo "$i" >> /home/%userprofile%/bin/check_sshd_log.txt
+         ssh -l %userprofile% -i /home/%userprofile%/.ssh/id_rsa $i '/usr/bin/sudo /usr/bin/egrep "(oracle|young|williams|walga|bari|kocha)" /etc/ssh/sshd_config' >> /home/%userprofile%/bin/check_sshd_log.txt
     done
