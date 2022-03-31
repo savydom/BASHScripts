63e8bff7e18beb6ba0ae2d2d7f56dce66d5ce3b2
savydom <46859196+savydom@users.noreply.github.com>
2022-03-31 14:14:09 -0400

    Update check_networker.sh

--- check_networker.sh
+++ check_networker.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
-HL=/home/mjohnson/bin/hostlist.txt
-echo 'Networker Check' > /home/mjohnson/bin/check_networker_log.txt
+HL=/home/%userprofile%/bin/hostlist.txt
+echo 'Networker Check' > /home/%userprofile%/bin/check_networker_log.txt
 for i in `/usr/bin/cat $HL`
 do
-  OST=$(ssh -q -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i "uname -s")
+  OST=$(ssh -q -l %userprofile% -i /home/%userprofile%/.ssh/id_rsa $i "uname -s")
   if [ $OST == "SunOS" ]; then
-    echo "$i" >> /home/mjohnson/bin/check_networker_log.txt
-    ssh -q -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i pkginfo -l LGTOclnt | egrep -i '(pkginst|version)' >> /home/mjohnson/bin/check_networker_log.txt
+    echo "$i" >> /home/%userprofile%/bin/check_networker_log.txt
+    ssh -q -l %userprofile% -i /home/%userprofile%/.ssh/id_rsa $i pkginfo -l LGTOclnt | egrep -i '(pkginst|version)' >> /home/%userprofile%/bin/check_networker_log.txt
   elif [ $OST == "Linux" ]; then
-    echo "$i" >> /home/mjohnson/bin/check_networker_log.txt
-    ssh -q -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i "/usr/bin/sudo yum -q list installed lgtoclnt" >> /home/mjohnson/bin/check_networker_log.txt
+    echo "$i" >> /home/%userprofile%/bin/check_networker_log.txt
+    ssh -q -l %userprofile% -i /home/%userprofile%/.ssh/id_rsa $i "/usr/bin/sudo yum -q list installed lgtoclnt" >> /home/%userprofile%/bin/check_networker_log.txt
   else
-    echo "$i $OST issues" >> /home/mjohnson/bin/check_networker_log.txt
+    echo "$i $OST issues" >> /home/%userprofile%/bin/check_networker_log.txt
   fi
 done
