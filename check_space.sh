85e2666fa82ac272fa20bf3887f70641e7e7dea6
savydom <46859196+savydom@users.noreply.github.com>
2022-03-31 14:20:29 -0400

    Update check_space.sh

--- check_space.sh
+++ check_space.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-HL=/home/mjohnson/bin/hostlist.txt
-echo 'Space Log' > /home/mjohnson/bin/check_space_log.txt
+HL=/home/%userprofile%/bin/hostlist.txt
+echo 'Space Log' > /home/%userprofile%/bin/check_space_log.txt
 for i in `/usr/bin/cat $HL`
     do
-         echo "$i" >> /home/mjohnson/bin/check_space_log.txt
-         ssh -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i df -ah >> /home/mjohnson/bin/check_space_log.txt
+         echo "$i" >> /home/%userprofile%/bin/check_space_log.txt
+         ssh -l %userprofile% -i /home/%userprofile%/.ssh/id_rsa $i df -ah >> /home/%userprofile%/bin/check_space_log.txt
     done
