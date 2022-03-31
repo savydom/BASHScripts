0d79b6a646293545430c29b93d0acc542db93c13
savydom <46859196+savydom@users.noreply.github.com>
2022-03-31 15:34:35 -0400

    Update ldap_search_soon_to_be_locked.sh

--- ldap_search_soon_to_be_locked.sh
+++ ldap_search_soon_to_be_locked.sh
@@ -1,6 +1,6 @@
-/usr/bin/ldapsearch -D "cn=Manager,dc=sscnola,dc=oob"  -b "ou=people,dc=sscnola,dc=oob" -h sscmgt2.sscnola.oob -p 636 -P /var/ldap/cert8.db  -s sub "(pwdChangedTime=*)"
+/usr/bin/ldapsearch -D "cn=Manager,dc=DC,dc=DCC"  -b "ou=people,dc=DC,dc=DCC" -h mgt2.DC.DCC -p 636 -P /var/ldap/cert8.db  -s sub "(pwdChangedTime=*)"
 
-/usr/bin/ldapsearch -LLL -h sscmgt2.sscnola.oob -p 636 -P /var/ldap/cert8.db -X -s base -b "ou=people,dc=sscnola,dc=oob" -h sscmgt2.sscnola.oob -p 636 -P /var/ldap/cert8.db  -s sub "(pwdChangedTime=*)"
+/usr/bin/ldapsearch -LLL -h mgt2.DC.DCC -p 636 -P /var/ldap/cert8.db -X -s base -b "ou=people,dc=DC,dc=DCC" -h mgt2.DC.DCC -p 636 -P /var/ldap/cert8.db  -s sub "(pwdChangedTime=*)"
 
 
 
