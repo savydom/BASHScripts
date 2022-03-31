18d382bc9ba39ca0286437792c1da12187ed390f
savydom <46859196+savydom@users.noreply.github.com>
2022-03-31 14:15:05 -0400

    Update check_passwd.sh

--- check_passwd.sh
+++ check_passwd.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-HL=/home/mjohnson/bin/hostlist.txt
-echo 'Passwd Log' > /home/mjohnson/bin/check_passwd_log.txt
+HL=/home/%userprofile%/bin/hostlist.txt
+echo 'Passwd Log' > /home/%userprofile%/bin/check_passwd_log.txt
 for i in `/usr/bin/cat $HL`
 do
-    echo "$i" >> /home/mjohnson/bin/check_passwd_log.txt
-    ssh -q -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i "egrep '(emerg|holden)' /etc/passwd"
+    echo "$i" >> /home/%userprofile%/bin/check_passwd_log.txt
+    ssh -q -l %userprofile% -i /home/%userprofile%/.ssh/id_rsa $i "egrep '(emerg|holden)' /etc/passwd"
 done
