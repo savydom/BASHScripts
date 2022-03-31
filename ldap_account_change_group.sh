229d5f28af1d7a9f4a6f993613566c6fbe374ad1
savydom <46859196+savydom@users.noreply.github.com>
2022-03-31 14:31:46 -0400

    Update ldap_account_change_group.sh

--- ldap_account_change_group.sh
+++ ldap_account_change_group.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-/usr/bin/ldapmodify -h sscmgt2.sscnola.oob -p 636 -P /var/ldap/cert8.db \
-	-D cn=Manager,dc=sscnola,dc=oob -w - <<!
-dn: gid=$1,ou=group,dc=sscnola,dc=oob
+/usr/bin/ldapmodify -h %ldap_server% -p 636 -P /var/ldap/cert8.db \
+	-D cn=Manager,dc=DC,dc=DCC -w - <<!
+dn: gid=$1,ou=group,dc=DC,dc=DCC
 changetype: modify
 add: memberuid
 memberuid: $1
 -
 !
-echo "tnguyen added to group: $1"
+echo "tommy added to group: $1"
 
 
 
