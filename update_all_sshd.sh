56146caddfeea2b82e1f710b3dc792a41bff4490
Rock Sales <46859196+savydom@users.noreply.github.com>
2022-04-06 14:35:04 -0400

    Update update_all_sshd.sh

--- update_all_sshd.sh
+++ update_all_sshd.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
-HL=/home/mjohnson/bin/hostlist.txt
-echo 'SSHD Log' > /home/mjohnson/bin/update_all_sshd_log.txt
+HL=/home/%currentuser%/bin/hostlist.txt
+echo 'SSHD Log' > /home/%currentuser%/bin/update_all_sshd_log.txt
 for i in `/usr/bin/cat $HL`
 do
-  OST=$(ssh -q -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i "uname -s")
+  OST=$(ssh -q -l %currentuser% -i /home/%currentuser%/.ssh/id_rsa $i "uname -s")
   if [ $OST == "SunOS" ]; then
-    echo "$i" >> /home/mjohnson/bin/update_all_sshd_log.txt
-    ssh -q -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i '/usr/local/bin/sudo /usr/bin/bash - /home/mjohnson/bin/update_sshd' >> /home/mjohnson/bin/update_all_sshd_log.txt
+    echo "$i" >> /home/%currentuser%/bin/update_all_sshd_log.txt
+    ssh -q -l %currentuser% -i /home/%currentuser%/.ssh/id_rsa $i '/usr/local/bin/sudo /usr/bin/bash - /home/%currentuser%/bin/update_sshd' >> /home/%currentuser%/bin/update_all_sshd_log.txt
   elif [ $OST == "Linux" ]; then
-    echo "$i" >> /home/mjohnson/bin/update_all_sshd_log.txt
-    ssh -q -l mjohnson -i /home/mjohnson/.ssh/id_rsa $i '/usr/bin/sudo /bin/bash - /home/mjohnson/bin/RHupdate_sshd' >> /home/mjohnson/bin/update_all_sshd_log.txt
+    echo "$i" >> /home/%currentuser%/bin/update_all_sshd_log.txt
+    ssh -q -l %currentuser% -i /home/%currentuser%/.ssh/id_rsa $i '/usr/bin/sudo /bin/bash - /home/%currentuser%/bin/RHupdate_sshd' >> /home/%currentuser%/bin/update_all_sshd_log.txt
   fi
 done
